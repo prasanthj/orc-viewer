08c4c23d6f397e18769c3ff94dee5b15cf42ae79
Prasanth Jayachandran <j.prasanth.j@gmail.com>
2015-05-16 00:53:48 -0700

    updated colors and added selection transitions

--- orc.js
+++ orc.js
@@ -1,3 +1,4 @@
+var fileView
 loadLayout()
 
 function loadLayout() {
@@ -41,7 +42,7 @@ function updateMetadataList(items) {
 
 function drawFileLayout(filedump, x, y, w, h) {
 	console.log("x: " + x + " y: " + y + " w: " + w + " h: " + h)
-	var fileView = d3.select(".file-layout")
+	fileView = d3.select(".file-layout")
 					.append("svg:svg")
 					.attr("width", w)
 					.attr("height", h);
@@ -70,15 +71,14 @@ function drawStripe(fileView, stripes, stripeId, x, y, w, h) {
 			.attr("y", y)
 			.attr("width", w)
 			.attr("height", h)
-			.attr("class", "stripe")
-			.on("click", function(){
-				// shrink the file and expand the stripe
-			});
+			.attr("id", "stripe")
+			.on("mouseover", focus)
+			.on("mouseout", defocus);
 
 	fileView.append("svg:text")
 			.attr("x", x + (w / 2))
 			.attr("y", y + (h / 2))
-			.attr("class", "inner-text")
+			.attr("id", "inner-text")
 			.text("Stripe " + stripeId);
 }
 
@@ -89,14 +89,35 @@ function drawFooter(fileView, filedump, x, y, w, h) {
 			.attr("y", y)
 			.attr("width", w)
 			.attr("height", h)
-			.attr("class", "footer")
-			.on("click", function(){
-				// shrink the file and expand the footer
-			});
+			.attr("id", "footer")
+			.on("mouseover", focus)
+			.on("mouseout", defocus);
 
 	fileView.append("svg:text")
 			.attr("x", x + (w / 2))
 			.attr("y", y + (h / 2))
-			.attr("class", "inner-text")
+			.attr("id", "inner-text")
 			.text("Footer");
+}
+
+function focus() {
+	var x = parseFloat(d3.select(this).attr("x"));
+	var y = parseFloat(d3.select(this).attr("y"));
+	var w = parseFloat(d3.select(this).attr("width"));
+	var h = parseFloat(d3.select(this).attr("height"));
+	fileView.append("svg:rect")
+		    .attr("x", x)
+		    .attr("y", y + h - 2)
+		    .attr("width", w)
+		    .attr("height", 3)
+		    .attr("class", "selected")
+		    .style("opacity", 0.0)
+		    .transition()
+			.delay(100)
+			.duration(500)
+			.style("opacity", 1.0);
+}
+
+function defocus() {
+	d3.select(".selected").remove();
 }
