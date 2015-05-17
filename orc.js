42d57eaa312d8b9c8a7b2ab2f7ff21e4196a8a3b
Prasanth Jayachandran <j.prasanth.j@gmail.com>
2015-05-17 03:10:13 -0700

    level 2 transitions added

--- orc.js
+++ orc.js
@@ -78,43 +78,17 @@ function drawFileLayout(filedump, x, y, w, h) {
 	var totalStripesHeight = 0.7 * h
 	var perStripeHeight = totalStripesHeight / nStripes
 	var stripeWidth = 0.5 * w
-	for (var i=0; i<nStripes; i++) {	
-		drawStripe(stripes, i + 1, newX, newY, stripeWidth, perStripeHeight)
+	for (var i=0; i<nStripes; i++) {
+		drawRect(fileView, newX, newY, stripeWidth, perStripeHeight, "stripe", elementClick, "Stripe " + (i + 1), i + 1)
 		newY += perStripeHeight
 	}
 
 	var footerHeight = 0.1 * h
-	drawFooter(filedump, newX, newY, stripeWidth, footerHeight)
+	drawRect(fileView, newX, newY, stripeWidth, footerHeight, "footer", elementClick, "Footer")
 }
 
 function zoomed() {
 	fileView.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-}
-
-function drawStripe(stripes, stripeId, x, y, w, h) {
-	var stripe = fileView.append("svg:rect")
-			.attr("x", x)
-			.attr("y", y)
-			.attr("width", w)
-			.attr("height", h)
-			.attr("class", "stripe")
-			.attr("id", stripeId)
-			.on("click", elementClick);
-
-	drawText("Stripe " + stripeId, x + (w / 2), y + (h / 2));
-}
-
-function drawFooter(filedump, x, y, w, h) {
-	fileView.append("svg:rect")
-			.attr("x", x)
-			.attr("y", y)
-			.attr("width", w)
-			.attr("height", h)
-			.attr("class", "footer")
-			.attr("id", "footer")
-			.on("click", elementClick);
-
-	drawText("Footer", x + (w / 2), y + (h / 2));
 }
 
 function elementClick() {
@@ -123,15 +97,16 @@ function elementClick() {
 	var y = parseFloat(d3.select(this).attr("y"));
 	var w = parseFloat(d3.select(this).attr("width"));
 	var h = parseFloat(d3.select(this).attr("height"));
-	drawSelectionLine(x, y, x + w, y)
-	drawSelectionLine(x, y + h, x + w, y + h)
+	drawLine(fileView, x, y, x + w, y, "selected", false);
+	drawLine(fileView, x, y + h, x + w, y + h, "selected", false);
 
 	if (d3.select(this).attr("class") == "stripe") {
 		var stripeId = d3.select(this).attr("id")
 		updateMetadataList(stripeMetadata[stripeId])
 		drawStripeExpanded(x, y, w, h)
 	} else if (d3.select(this).attr("class") == "footer") {
-		updateMetadataList(fileMetadata)	
+		updateMetadataList(fileMetadata)
+		drawFooterExpanded(x, y, w, h)
 	}
 
 	// stop the click event from propagting to parent file-layout
@@ -148,76 +123,130 @@ function layoutClick() {
 	updateMetadataList(fileMetadata)
 }
 
-function drawStripeExpanded(x, y, w, h) {
+function drawFooterExpanded(x, y, w, h) {
 	fileView.transition()
 			.delay(100)
 			.duration(500)
 	        .attr("transform", "scale(0.5)");
+	x = 0.5 * x;
+	y = 0.5 * y;
+	w = 0.5 * w;
+	h = 0.5 * h;
 
 	d3.selectAll(".depth-1").remove()
+	var depth1 = fileView.append("g")
+						 .attr("transform", "scale(2)");
 
+	var stripeWidth = width * 0.5
+	var stripeHeight = height * 0.5
 	var x1 = x + w
 	var y1 = y
 	var lineWidth = 100
 	var x2 = x1 + 100
 	var y2 = y1
-	drawLine(x1, y1, x2, y2)
+	drawLine(depth1, x1, y1, x2, y2, "expander-line depth-1", true)
 	var x3 = x1
 	var y3 = y1 + h
 	var x4 = x1 + lineWidth
-	var y4 = y1 + height
-	drawLine(x3, y3, x4, y4)
+	var y4 = y1 + stripeHeight
+	drawLine(depth1, x3, y3, x4, y4, "expander-line depth-1", true)
 
-	var headerHeight = 0.15 * height
-	var footerHeight = 0.15 * height
-	var bodyHeight = height - headerHeight - footerHeight
+	var headerHeight = 0.4 * stripeHeight
+	var footerHeight = 0.2 * stripeHeight
+	var bodyHeight = stripeHeight - headerHeight - footerHeight
+	drawRect(depth1, x2, y2, stripeWidth, headerHeight, "stripe-header depth-1", elementClick, "Metadata")
+	drawRect(depth1, x2, y2 + headerHeight, stripeWidth, bodyHeight, "stripe-body depth-1", elementClick, "Footer")
+	drawRect(depth1, x2, y2 + headerHeight + bodyHeight, stripeWidth, footerHeight, "stripe-footer depth-1", elementClick, "Postscript")
+}
 
-	var stripeWidth = 500
-	fileView.append("svg:rect")
-			.attr("x", x2)
-			.attr("y", y2)
-			.attr("width", width)
-			.attr("height", height)
-			.attr("class", "stripe-expanded depth-1")
-			.on("click", elementClick)
+function drawStripeExpanded(x, y, w, h) {
+	fileView.transition()
+			.delay(100)
+			.duration(500)
+	        .attr("transform", "scale(0.5)");
+	x = 0.5 * x;
+	y = 0.5 * y;
+	w = 0.5 * w;
+	h = 0.5 * h;
+
+	d3.selectAll(".depth-1").remove()
+	var depth1 = fileView.append("g")
+						 .attr("transform", "scale(2)");
+
+	var stripeWidth = width * 0.5
+	var stripeHeight = height * 0.5
+	var x1 = x + w
+	var y1 = y
+	var lineWidth = 100
+	var x2 = x1 + 100
+	var y2 = y1
+	drawLine(depth1, x1, y1, x2, y2, "expander-line depth-1", true)
+	var x3 = x1
+	var y3 = y1 + h
+	var x4 = x1 + lineWidth
+	var y4 = y1 + stripeHeight
+	drawLine(depth1, x3, y3, x4, y4, "expander-line depth-1", true)
+
+	var headerHeight = 0.15 * stripeHeight
+	var footerHeight = 0.15 * stripeHeight
+	var bodyHeight = stripeHeight - headerHeight - footerHeight
+	drawRect(depth1, x2, y2, stripeWidth, headerHeight, "stripe-header depth-1", elementClick, "Indexes")
+	drawRect(depth1, x2, y2 + headerHeight, stripeWidth, bodyHeight, "stripe-body depth-1", elementClick, "Data")
+	drawRect(depth1, x2, y2 + headerHeight + bodyHeight, stripeWidth, footerHeight, "stripe-footer depth-1", elementClick, "Stripe Footer")
+}
+
+function drawRect(node, x, y, w, h, klass, clickHandler, text) {
+	drawRect(node, x, y, w, h, klass, clickHandler, text, "")
+}
+
+function drawRect(node, x, y, w, h, klass, clickHandler, text, id) {
+	node.append("svg:rect")
+			.attr("x", x)
+			.attr("y", y)
+			.attr("width", w)
+			.attr("height", h)
+			.attr("class", klass)
+			.attr("id", id)
+			.on("click", clickHandler)
 			.style("opacity", "0")
 			.transition()
 			.delay(100)
 			.duration(500)
 			.style("opacity", "1");
+	drawText(node, text, x + (w / 2), y + (h / 2))
 }
 
-function drawLine(x1, y1, x2, y2) {
-	fileView.append("svg:line")
+function drawLine(node, x1, y1, x2, y2, klass, isDotted) {
+	if (isDotted) {
+		node.append("svg:line")
 	        .attr("x1", x1)
 	        .attr("y1", y1)
 	        .attr("x2", x2)
 	        .attr("y2", y2)
-	        .attr("class", "expander-line depth-1")
+	        .attr("class", klass)
 	        .style("stroke-dasharray", ("5, 5"))
 	        .style("opacity", "0")
 	        .transition()
 			.delay(100)
 			.duration(500)
-			.style("opacity", "1");
-}
-
-function drawSelectionLine(x1, y1, x2, y2) {
-	fileView.append("svg:line")
+			.style("opacity", "0.5");
+	} else {
+		node.append("svg:line")
 	        .attr("x1", x1)
 	        .attr("y1", y1)
 	        .attr("x2", x2)
 	        .attr("y2", y2)
-	        .attr("class", "selected")
+	        .attr("class", klass)
 	        .style("opacity", "0")
 	        .transition()
 			.delay(100)
 			.duration(500)
-			.style("opacity", "1");
+			.style("opacity", "1");	
+	}
 }
 
-function drawText(t, x, y) {
-	fileView.append("svg:text")
+function drawText(node, t, x, y) {
+	node.append("svg:text")
 			.attr("x", x)
 			.attr("y", y)
 			.attr("class", "inner-text")
