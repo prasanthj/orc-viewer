db84d53c771470ba8b0d3d80231d316fc8d933c7
Prasanth Jayachandran <j.prasanth.j@gmail.com>
2015-05-17 01:37:43 -0700

    stripe expander, layout zoom added

--- orc.js
+++ orc.js
@@ -1,19 +1,17 @@
 var fileView
 var fileMetadata = []
 var stripeMetadata = {}
-var footerMetadata = []
+var width = $(".col-md-8").width();
+var height = $(".body.container-fluid").height();
 loadLayout()
 
 function loadLayout() {
 	populateFileMetadata(data);
 	populateStripeMetadata(data);
-	populateFooterMetadata(data);
 	updateMetadataList(fileMetadata);
 
 	var originX = $(".col-md-4").width();
 	var originY = 0;
-	var width = $(".col-md-8").width();
-	var height = $(".body.container-fluid").height();
 	drawFileLayout(data, originX, originY, width, height);
 }
 
@@ -34,6 +32,7 @@ function populateStripeMetadata(filedump) {
 	for(var i=0; i<stripes.length; i++) {
 		var stripeInfo = []
 		var stripe = stripes[i]
+		stripeInfo.push(["Stripe " + stripe["stripeNumber"], ""])
         stripeInfo.push(["Offset", stripe["stripeInformation"]["offset"]])
         stripeInfo.push(["Index Length", stripe["stripeInformation"]["indexLength"]])
         stripeInfo.push(["Data Length", stripe["stripeInformation"]["dataLength"]])
@@ -41,10 +40,6 @@ function populateStripeMetadata(filedump) {
         stripeInfo.push(["Row Count", stripe["stripeInformation"]["rowCount"]])
         stripeMetadata[stripe["stripeNumber"]] = stripeInfo
 	}
-}
-
-function populateFooterMetadata(filedump) {
-
 }
 
 function htmlEntities(str) {
@@ -62,11 +57,18 @@ function updateMetadataList(items) {
 
 function drawFileLayout(filedump, x, y, w, h) {
 	//console.log("x: " + x + " y: " + y + " w: " + w + " h: " + h)
+ 	var zoom = d3.behavior.zoom()
+ 				 .scaleExtent([-8, 8])
+ 				 .on("zoom", zoomed);
+
 	fileView = d3.select(".file-layout")
 					.append("svg:svg")
 					.attr("width", w)
 					.attr("height", h)
-					.on("click", layoutClick);
+					.on("click", layoutClick)
+					.append("g")
+					.attr("transform", "scale(1)")
+    				.call(zoom);
 
 	var stripes = filedump["stripes"]
 	var nStripes = filedump["stripes"].length
@@ -77,16 +79,19 @@ function drawFileLayout(filedump, x, y, w, h) {
 	var perStripeHeight = totalStripesHeight / nStripes
 	var stripeWidth = 0.5 * w
 	for (var i=0; i<nStripes; i++) {	
-		drawStripe(fileView, stripes, i + 1, newX, newY, stripeWidth, perStripeHeight)
+		drawStripe(stripes, i + 1, newX, newY, stripeWidth, perStripeHeight)
 		newY += perStripeHeight
 	}
 
 	var footerHeight = 0.1 * h
-	drawFooter(fileView, filedump, newX, newY, stripeWidth, footerHeight)
+	drawFooter(filedump, newX, newY, stripeWidth, footerHeight)
 }
 
-function drawStripe(fileView, stripes, stripeId, x, y, w, h) {
-	//console.log("x: " + x + " y: " + y + " w: " + w + " h: " + h)
+function zoomed() {
+	fileView.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+}
+
+function drawStripe(stripes, stripeId, x, y, w, h) {
 	var stripe = fileView.append("svg:rect")
 			.attr("x", x)
 			.attr("y", y)
@@ -96,15 +101,10 @@ function drawStripe(fileView, stripes, stripeId, x, y, w, h) {
 			.attr("id", stripeId)
 			.on("click", elementClick);
 
-	fileView.append("svg:text")
-			.attr("x", x + (w / 2))
-			.attr("y", y + (h / 2))
-			.attr("class", "inner-text")
-			.text("Stripe " + stripeId);
+	drawText("Stripe " + stripeId, x + (w / 2), y + (h / 2));
 }
 
-function drawFooter(fileView, filedump, x, y, w, h) {
-	//console.log("x: " + x + " y: " + y + " w: " + w + " h: " + h)
+function drawFooter(filedump, x, y, w, h) {
 	fileView.append("svg:rect")
 			.attr("x", x)
 			.attr("y", y)
@@ -114,35 +114,24 @@ function drawFooter(fileView, filedump, x, y, w, h) {
 			.attr("id", "footer")
 			.on("click", elementClick);
 
-	fileView.append("svg:text")
-			.attr("x", x + (w / 2))
-			.attr("y", y + (h / 2))
-			.attr("class", "inner-text")
-			.text("Footer");
+	drawText("Footer", x + (w / 2), y + (h / 2));
 }
 
 function elementClick() {
-	d3.select(".selected").remove();
+	d3.selectAll(".selected").remove();
 	var x = parseFloat(d3.select(this).attr("x"));
 	var y = parseFloat(d3.select(this).attr("y"));
 	var w = parseFloat(d3.select(this).attr("width"));
 	var h = parseFloat(d3.select(this).attr("height"));
-	fileView.append("svg:rect")
-		    .attr("x", x)
-		    .attr("y", y + h - 2)
-		    .attr("width", w)
-		    .attr("height", 3)
-		    .attr("class", "selected")
-		    .style("opacity", 0.0)
-		    .transition()
-			.delay(100)
-			.duration(200)
-			.style("opacity", 1.0);
+	drawSelectionLine(x, y, x + w, y)
+	drawSelectionLine(x, y + h, x + w, y + h)
 
 	if (d3.select(this).attr("class") == "stripe") {
 		var stripeId = d3.select(this).attr("id")
 		updateMetadataList(stripeMetadata[stripeId])
+		drawStripeExpanded(x, y, w, h)
 	} else if (d3.select(this).attr("class") == "footer") {
+		updateMetadataList(fileMetadata)	
 	}
 
 	// stop the click event from propagting to parent file-layout
@@ -150,6 +139,87 @@ function elementClick() {
 }
 
 function layoutClick() {
-	d3.select(".selected").remove();
+	fileView.transition()
+			.delay(100)
+			.duration(500)
+	        .attr("transform", "scale(1)");
+	d3.selectAll(".selected").remove();
+	d3.selectAll(".depth-1").remove();
 	updateMetadataList(fileMetadata)
+}
+
+function drawStripeExpanded(x, y, w, h) {
+	fileView.transition()
+			.delay(100)
+			.duration(500)
+	        .attr("transform", "scale(0.5)");
+
+	d3.selectAll(".depth-1").remove()
+
+	var x1 = x + w
+	var y1 = y
+	var lineWidth = 100
+	var x2 = x1 + 100
+	var y2 = y1
+	drawLine(x1, y1, x2, y2)
+	var x3 = x1
+	var y3 = y1 + h
+	var x4 = x1 + lineWidth
+	var y4 = y1 + height
+	drawLine(x3, y3, x4, y4)
+
+	var headerHeight = 0.15 * height
+	var footerHeight = 0.15 * height
+	var bodyHeight = height - headerHeight - footerHeight
+
+	var stripeWidth = 500
+	fileView.append("svg:rect")
+			.attr("x", x2)
+			.attr("y", y2)
+			.attr("width", width)
+			.attr("height", height)
+			.attr("class", "stripe-expanded depth-1")
+			.on("click", elementClick)
+			.style("opacity", "0")
+			.transition()
+			.delay(100)
+			.duration(500)
+			.style("opacity", "1");
+}
+
+function drawLine(x1, y1, x2, y2) {
+	fileView.append("svg:line")
+	        .attr("x1", x1)
+	        .attr("y1", y1)
+	        .attr("x2", x2)
+	        .attr("y2", y2)
+	        .attr("class", "expander-line depth-1")
+	        .style("stroke-dasharray", ("5, 5"))
+	        .style("opacity", "0")
+	        .transition()
+			.delay(100)
+			.duration(500)
+			.style("opacity", "1");
+}
+
+function drawSelectionLine(x1, y1, x2, y2) {
+	fileView.append("svg:line")
+	        .attr("x1", x1)
+	        .attr("y1", y1)
+	        .attr("x2", x2)
+	        .attr("y2", y2)
+	        .attr("class", "selected")
+	        .style("opacity", "0")
+	        .transition()
+			.delay(100)
+			.duration(500)
+			.style("opacity", "1");
+}
+
+function drawText(t, x, y) {
+	fileView.append("svg:text")
+			.attr("x", x)
+			.attr("y", y)
+			.attr("class", "inner-text")
+			.text(t);
 }
